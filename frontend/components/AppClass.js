21e958a496f6e90d1a20ba1db5353cd328f729d0
Sean-Ubele <seanjohnreedubeleknuckles@gmail.com>
2022-04-15 16:41:07 -0400

    MVP#1 The Grid- Done MVP#2 The Tests-WIP

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,37 +1,162 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
 
-export default class AppClass extends React.Component {
-  render() {
-    const { className } = this.props
+const url = 'http://localhost:9000/api/result';
+
+const initialState = {
+  x: 2,
+  y: 2,
+  steps: 0,
+  successMessage: '',
+  errorMessage: '',
+  email: '',
+  grid: [
+    null, null, null,
+    null, 'B', null,
+    null, null, null
+  ],
+
+}
+export default class AppClass extends React.Component{
+  
+  state = initialState;
+
+  newArray = () => {
+
+    if(this.state.x === 1 && this.state.y === 1) {
+      return ['B', null, null, null, null, null, null, null, null,]}
+      else if (this.state.x === 2 && this.state.y === 1){return [null, 'B', null, null, null, null, null, null, null,]}
+      else if (this.state.x === 3 && this.state.y === 1){return [null, null, 'B', null, null, null, null, null, null,]}
+      else if (this.state.x === 1 && this.state.y === 2){return [null, null, null, 'B', null, null, null, null, null,]}
+      else if (this.state.x === 1 && this.state.y === 3){return [null, null, null, null, null, null, 'B', null, null,]}
+      else if (this.state.x === 2 && this.state.y === 2){return [null, null, null, null, 'B', null, null, null, null,]}
+      else if (this.state.x === 2 && this.state.y === 3){return [null, null, null, null, null, null, null, 'B', null,]}
+      else if (this.state.x === 3 && this.state.y === 2){return [null, null, null, null, null, 'B', null, null, null,]}
+      else if (this.state.x === 3 && this.state.y === 3){return [null, null, null, null, null, null, null, null, 'B',]}
+      else { return 'how did you do that?' }
+
+  }
+
+  componentDidUpdate(oldProps, oldState) {
+    if (oldState.x !== this.state.x || oldState.y !== this.state.y) {
+      this.setState({...this.state, grid: this.newArray()})
+    } else {
+      console.log('current')
+    }
+  }
+
+   moveLeft = () => {
+
+    let counterX = this.state.x - 1;
+    let stepCounter = this.state.steps + 1;
+
+    if(this.state.x > 1) {
+      this.setState({...this.state, x: counterX, steps: stepCounter, successMessage: '', errorMessage: ''})
+    } else {
+      this.setState({...this.state, successMessage: '', errorMessage: `y u go left, this isn't pokemon there aren't any creepypastas outside of the bounds!`})
+    }
+  }
+
+   moveRight = () => {
+
+    let counterX = this.state.x + 1;
+    let stepCounter = this.state.steps + 1;
+
+    if(this.state.x < 3) {
+      this.setState({...this.state, x: counterX, steps: stepCounter, successMessage: '', errorMessage: ''})
+    } else {
+      this.setState({...this.state, successMessage: '', errorMessage: `y u go right, this isn't zelda there aren't any creepypastas outside of the bounds!`})
+    }
+  }
+  
+   moveUp = () => {
+
+    let counterY = this.state.y - 1;
+    let stepCounter = this.state.steps + 1;
+
+    if(this.state.y > 1) {
+      this.setState({...this.state, y: counterY, steps: stepCounter, successMessage: '', errorMessage: ''})
+    } else {
+      this.setState({...this.state, successMessage: '', errorMessage: `Why did you try to go up? The only thing up there is Lavender Town`})
+    }
+  }
+
+   moveDown = () => {
+
+    let counterY = this.state.y + 1;
+    let stepCounter = this.state.steps + 1;
+
+    if(this.state.y < 3) {
+      this.setState({...this.state, y: counterY, steps: stepCounter, successMessage: '', errorMessage: ''})
+    } else {
+      this.setState({...this.state, successMessage: '', errorMessage: `Why did you try to go down? The only thing down there is where Ben Drowned`})
+    }
+  }
+
+   reset = () => {
+    this.setState(initialState)
+  }
+  
+   changeInput = (event) => {
+    const { value } = event.target
+    this.setState ({...this.state, email: value})
+  }
+
+   onSubmit = (event) => {
+
+    event.preventDefault()
+
+    const newData = { 'x': this.state.x, 'y': this.state.y, 'steps': this.state.steps, 'email': this.state.email }
+
+    axios.post(url, newData)
+      .then(response => {
+        this.setState({
+          ...this.state, 
+          successMessage: [...this.state.successMessage, response.data.message],
+          errorMessage: '',
+          email: ''
+        })
+      })
+      .catch(error => {
+        console.error(error)
+        this.setState({
+          ...this.state, 
+          successMessage: '',
+          errorMessage: error.response.data.message,
+          email: ''
+        })
+      })
+  }
+
+  render() { 
+
+    const {className} = this.props
+
     return (
       <div id="wrapper" className={className}>
         <div className="info">
-          <h3 id="coordinates">Coordinates (2, 2)</h3>
-          <h3 id="steps">You moved 0 times</h3>
+          <h3 id="coordinates">Coordinates ({this.state.x}, {this.state.y})</h3>
+          <h3 id="steps">You moved {this.state.steps} times</h3>
         </div>
         <div id="grid">
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square active">B</div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
+          { this.state.grid.map(location => {
+            if(location === 'B') {
+              return <div className='square active'>B</div>
+            } else { return <div className='square'/> }
+          })}
         </div>
         <div className="info">
-          <h3 id="message"></h3>
+          <h3 id="message">{this.state.errorMessage}{this.state.successMessage}</h3>
         </div>
         <div id="keypad">
-          <button id="left">LEFT</button>
-          <button id="up">UP</button>
-          <button id="right">RIGHT</button>
-          <button id="down">DOWN</button>
-          <button id="reset">reset</button>
+          <button id="left" onClick={() => this.moveLeft()}>LEFT</button>
+          <button id="up" onClick={() => this.moveUp()}>UP</button>
+          <button id="right" onClick={() => this.moveRight()}>RIGHT</button>
+          <button id="down" onClick={() => this.moveDown()}>DOWN</button>
+          <button id="reset" onClick={() => this.reset()}>reset</button>
         </div>
-        <form>
-          <input id="email" type="email" placeholder="type email"></input>
+        <form onSubmit={this.onSubmit}>
+          <input id="email" type="email" placeholder="type email" value={this.state.email} onChange={this.changeInput}></input>
           <input id="submit" type="submit"></input>
         </form>
       </div>
--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,35 +1,152 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
+const url = 'http://localhost:9000/api/result';
+
+const initialState = {
+  x: 2,
+  y: 2,
+  steps: 0,
+  successMessage: '',
+  errorMessage: '',
+  email: '',
+  grid: [
+    null, null, null,
+    null, 'B', null,
+    null, null, null
+  ],
+
+}
 export default function AppFunctional(props) {
+  
+  const [state, setState] = useState(initialState)
+
+  const newArray = () => {
+
+    if(state.x === 1 && state.y === 1) {
+      return ['B', null, null, null, null, null, null, null, null,]}
+      else if (state.x === 2 && state.y === 1){return [null, 'B', null, null, null, null, null, null, null,]}
+      else if (state.x === 3 && state.y === 1){return [null, null, 'B', null, null, null, null, null, null,]}
+      else if (state.x === 1 && state.y === 2){return [null, null, null, 'B', null, null, null, null, null,]}
+      else if (state.x === 1 && state.y === 3){return [null, null, null, null, null, null, 'B', null, null,]}
+      else if (state.x === 2 && state.y === 2){return [null, null, null, null, 'B', null, null, null, null,]}
+      else if (state.x === 2 && state.y === 3){return [null, null, null, null, null, null, null, 'B', null,]}
+      else if (state.x === 3 && state.y === 2){return [null, null, null, null, null, 'B', null, null, null,]}
+      else if (state.x === 3 && state.y === 3){return [null, null, null, null, null, null, null, null, 'B',]}
+      else { return 'how did you do that?' }
+
+  }
+
+  useEffect(() => setState({...state, grid: newArray()}), [state.x, state.y]);
+
+  const moveLeft = () => {
+
+    let counterX = state.x - 1;
+    let stepCounter = state.steps + 1;
+
+    if(state.x > 1) {
+      setState({...state, x: counterX, steps: stepCounter, successMessage: '', errorMessage: ''})
+    } else {
+      setState({...state, successMessage: '', errorMessage: `y u go left, this isn't pokemon there aren't any creepypastas outside of the bounds!`})
+    }
+  }
+
+  const moveRight = () => {
+
+    let counterX = state.x + 1;
+    let stepCounter = state.steps + 1;
+
+    if(state.x < 3) {
+      setState({...state, x: counterX, steps: stepCounter, successMessage: '', errorMessage: ''})
+    } else {
+      setState({...state, successMessage: '', errorMessage: `y u go right, this isn't zelda there aren't any creepypastas outside of the bounds!`})
+    }
+  }
+  
+  const moveUp = () => {
+
+    let counterY = state.y - 1;
+    let stepCounter = state.steps + 1;
+
+    if(state.y > 1) {
+      setState({...state, y: counterY, steps: stepCounter, successMessage: '', errorMessage: ''})
+    } else {
+      setState({...state, successMessage: '', errorMessage: `Why did you try to go up? The only thing up there is Lavender Town`})
+    }
+  }
+
+  const moveDown = () => {
+
+    let counterY = state.y + 1;
+    let stepCounter = state.steps + 1;
+
+    if(state.y < 3) {
+      setState({...state, y: counterY, steps: stepCounter, successMessage: '', errorMessage: ''})
+    } else {
+      setState({...state, successMessage: '', errorMessage: `Why did you try to go down? The only thing down there is where Ben Drowned`})
+    }
+  }
+
+  const reset = () => {
+    setState(initialState)
+  }
+  
+  const changeInput = (event) => {
+    const { value } = event.target
+    setState ({...state, email: value})
+  }
+
+  const onSubmit = (event) => {
+
+    event.preventDefault()
+
+    const newData = { 'x': state.x, 'y': state.y, 'steps': state.steps, 'email': state.email }
+
+    axios.post(url, newData)
+      .then(response => {
+        setState({
+          ...state, 
+          successMessage: [...state.successMessage, response.data.message],
+          errorMessage: '',
+          email: ''
+        })
+      })
+      .catch(error => {
+        console.error(error)
+        setState({
+          ...state, 
+          successMessage: '',
+          errorMessage: error.response.data.message,
+          email: ''
+        })
+      })
+  }
+
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">Coordinates (2, 2)</h3>
-        <h3 id="steps">You moved 0 times</h3>
+        <h3 id="coordinates">Coordinates ({state.x}, {state.y})</h3>
+        <h3 id="steps">You moved {state.steps} times</h3>
       </div>
       <div id="grid">
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square active">B</div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
+        { state.grid.map(location => {
+          if(location === 'B') {
+            return <div className='square active'>B</div>
+          } else { return <div className='square'/> }
+        })}
       </div>
       <div className="info">
-        <h3 id="message"></h3>
+        <h3 id="message">{state.errorMessage}{state.successMessage}</h3>
       </div>
       <div id="keypad">
-        <button id="left">LEFT</button>
-        <button id="up">UP</button>
-        <button id="right">RIGHT</button>
-        <button id="down">DOWN</button>
-        <button id="reset">reset</button>
+        <button id="left" onClick={() => moveLeft()}>LEFT</button>
+        <button id="up" onClick={() => moveUp()}>UP</button>
+        <button id="right" onClick={() => moveRight()}>RIGHT</button>
+        <button id="down" onClick={() => moveDown()}>DOWN</button>
+        <button id="reset" onClick={() => reset()}>reset</button>
       </div>
-      <form>
-        <input id="email" type="email" placeholder="type email"></input>
+      <form onSubmit={onSubmit}>
+        <input id="email" type="email" placeholder="type email" value={state.email} onChange={changeInput}></input>
         <input id="submit" type="submit"></input>
       </form>
     </div>
